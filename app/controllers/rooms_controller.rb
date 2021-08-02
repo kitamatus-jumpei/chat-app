4a0fdef42224a9e7044788c471ddedf56d657629
kitamatsu junpei <umiwashi-teilu-z@ezweb.ne.jp>
2021-08-02 15:15:39 +0900

    メッセージの表示

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -13,25 +13,21 @@ class RoomsController < ApplicationController
   end
 
   def index
-    @message = Message.new
-    @room = Room.find(params[:room_id])
+    
   end
 
   def create
-    @room = Room.find(params[:room_id])
-    @message = @room.messages.new(message_params)
-    if @message.save
-      redirect_to room_messages_path(@room)
+    @room = Room.new(room_params)
+    if @room.save
+      redirect_to root_path
     else
-      render :index
+      render :new
     end
   end
 
   private
 
-  def message_params
-    params.require(:message).permit(:content).merge(user_id: current_user.id)
-  end
+  
 
   def room_params
     params.require(:room).permit(:name, user_ids: [])
